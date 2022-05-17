c4edd1fce897c944331f1aadf21a2572d1645499
lotus <daniel.younghwan.lee@gmail.com>
2022-05-17 17:43:05 -0400

    forced commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,40 @@
-console.log("hello world")
+var modal = document.getElementById("modal1")
+
+var getCountryInfo = function (countryName) {
+    var apiUrl = "https://disease.sh/v3/covid-19/countries/" + countryName;
+    fetch(apiUrl).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                console.log(data);
+            });
+        }
+    })
+        .catch(function (error) {
+            modal.style.display = "block";
+            
+            window.onclick = function (event) {
+                modal.style.display = "none";
+            }
+        })
+};
+
+var getStateInfo = function (stateName) {
+    var apiUrl = "https://disease.sh/v3/covid-19/states/" + stateName;
+    fetch(apiUrl).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                console.log(data);
+            });
+        }
+    })
+        .catch(function (error) {
+            modal.style.display = "block";
+
+            window.onclick = function (event) {
+                modal.style.display = "none";
+            }
+        })
+};
+
+getCountryInfo("Japan");
+getStateInfo("Wyoming");
